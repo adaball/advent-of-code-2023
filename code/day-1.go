2663734ed8fa78747b8426a131e5d24505d1690a
Adam Ball <etc@adamball.org>
2023-12-13 16:39:06 -0600

    Day 1 part 2

--- code/day-1.go
+++ code/day-1.go
@@ -7,11 +7,13 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	lines := readInput()
 	partOne(lines)
+	partTwo(lines)
 }
 
 func readInput() []string {
@@ -39,12 +41,91 @@ func readInput() []string {
 	return lines
 }
 
+// this is a much more involved way of solving it, but I somehow wasn't getting any success using regex alone 🤷‍
+//
+// nb: integers 48-57 inclusive are decimal 0-9 in utf8
+func findFirstAndLastDigit(line string) (string, string) {
+	wordToDigit := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	digitWords := regexp.MustCompile("one|two|three|four|five|six|seven|eight|nine")
+
+	// move 2 pointers towards the center of line (one starting at 0, one starting at len(line)-1)
+	// when a single digit or a spelled out number is found for each, return the found strings
+	finalA, finalB := "", ""
+	for ptrA, ptrB := 0, len(line)-1; ptrB >= 0 && ptrA < len(line); ptrA, ptrB = ptrA+1, ptrB-1 {
+
+		if finalA == "" && ptrA < len(line) {
+			charA := line[ptrA]
+			strA := line[0 : ptrA+1]
+			aMatches := digitWords.FindAllString(strA, -1)
+
+			if charA >= 48 && charA <= 57 {
+				finalA = fmt.Sprintf("%c", charA)
+			} else if aMatches != nil {
+				finalA = aMatches[0]
+			}
+		}
+
+		if finalB == "" && ptrB >= 0 {
+			charB := line[ptrB]
+			strB := line[ptrB:]
+			bMatches := digitWords.FindAllString(strB, -1)
+
+			if charB >= 48 && charB <= 57 {
+				finalB = fmt.Sprintf("%c", charB)
+			} else if bMatches != nil {
+				finalB = bMatches[len(bMatches)-1]
+			}
+		}
+	}
+
+	v, ok := wordToDigit[finalA]
+	if ok {
+		finalA = strings.Clone(v)
+	}
+
+	v, ok = wordToDigit[finalB]
+	if ok {
+		finalB = strings.Clone(v)
+	}
+
+	return finalA, finalB
+}
+
+func partTwo(lines []string) {
+	total := int64(0)
+	for _, line := range lines {
+		digitA, digitB := findFirstAndLastDigit(line)
+		combined := fmt.Sprintf("%s%s", digitA, digitB)
+		converted, err := strconv.ParseInt(combined, 10, 64)
+
+		if err != nil {
+			log.Fatalf("error converting %s: %v", combined, err)
+		}
+
+		total += converted
+	}
+
+	log.Printf("Part 2: %d", total)
+}
+
 func partOne(lines []string) {
 	lineMatches := make([][]string, 0)
 	re := regexp.MustCompile("\\d")
 	for _, line := range lines {
 		found := re.FindAllString(line, -1)
-		lineMatches = append(lineMatches, found)
+		if found != nil {
+			lineMatches = append(lineMatches, found)
+		}
 	}
 
 	total := int64(0)
@@ -58,9 +139,8 @@ func partOne(lines []string) {
 			log.Fatalf("error converting %s to int: %v", combo, err)
 		}
 
-		log.Printf("%d + %d = %d", total, comboInt, total+comboInt)
 		total = total + comboInt
 	}
 
-	log.Printf("part 1 %d", total)
+	log.Printf("Part 1: %d", total)
 }
